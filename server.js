09a8bd26d82cbbd7538bb7c7a6616d957a9060ab
M1keTrike <miguemolina4570@gmail.com>
2024-06-23 10:33:06 -0600

    [UPDATE]

--- server.js
+++ server.js
@@ -48,3 +48,7 @@ https.createServer(options, app).listen(8080, () => {
 });
 
 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
