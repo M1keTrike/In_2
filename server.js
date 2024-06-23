2d249ace399fe5ec730d345fd7822ec8d407f57a
M1keTrike <miguemolina4570@gmail.com>
2024-06-23 09:37:49 -0600

    [UPDATE] only https

--- server.js
+++ server.js
@@ -47,7 +47,4 @@ https.createServer(options, app).listen(8080, () => {
   console.log("Servidor HTTPS corriendo en el puerto 8080");
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+
