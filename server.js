04f49567357fcc40ff9b08b138ddc283d7dfbfb5
M1keTrike <miguemolina4570@gmail.com>
2024-07-03 15:03:39 -0600

    [UPDATE] much requests

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
 
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs:60 * 1000, // 15 minutes
   
-  max: 1000, // limit each IP to 100 requests per windowMs
+  max: 10000, // limit each IP to 100 requests per windowMs
   
   message: 'Too many requests from this IP, please try again later.',
   
