2463bb76e5dd6c3a1e91a34667f248d0f45ae78f
M1keTrike <miguemolina4570@gmail.com>
2024-07-16 00:45:33 -0600

    [UPDATE]

--- app/controllers/ventas.controller.js
+++ app/controllers/ventas.controller.js
@@ -1,119 +1,118 @@
 const Venta = require("../models/ventas.model.js");
 
 exports.create = (req, res) => {
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!",
-        });
-    }
-
-    const venta = new Venta({
-        fecha: req.body.fecha,
-        cliente: req.body.cliente,
-        estatus: req.body.estatus,
-        detalles: req.body.detalles,
-        ingresos: req.body.ingresos,
-        id_admin: req.body.id_admin,
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!",
     });
+  }
 
-    Venta.create(venta, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the Venta.",
-            });
-        else res.send(data);
-    });
+  const venta = new Venta({
+    cliente: req.body.cliente,
+    estatus: req.body.estatus,
+    detalles: req.body.detalles,
+    ingresos: req.body.ingresos,
+    id_admin: req.body.id_admin,
+  });
+
+  Venta.create(venta, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Venta.",
+      });
+    else res.send(data);
+  });
 };
 
 exports.findAll = (req, res) => {
-    const fecha = req.query.fecha;
+  const fecha = req.query.fecha;
 
-    Venta.getAll(fecha, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving ventas.",
-            });
-        else res.send(data);
-    });
+  Venta.getAll(fecha, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving ventas.",
+      });
+    else res.send(data);
+  });
 };
 
 exports.findOne = (req, res) => {
-    Venta.findById(req.params.id, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Not found Venta with id ${req.params.id}.`,
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving Venta with id " + req.params.id,
-                });
-            }
-        } else res.send(data);
-    });
+  Venta.findById(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Venta with id ${req.params.id}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Venta with id " + req.params.id,
+        });
+      }
+    } else res.send(data);
+  });
 };
 
 exports.update = (req, res) => {
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!",
-        });
-    }
-
-    Venta.updateById(req.params.id, new Venta(req.body), (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Not found Venta with id ${req.params.id}.`,
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error updating Venta with id " + req.params.id,
-                });
-            }
-        } else res.send(data);
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!",
     });
+  }
+
+  Venta.updateById(req.params.id, new Venta(req.body), (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Venta with id ${req.params.id}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Error updating Venta with id " + req.params.id,
+        });
+      }
+    } else res.send(data);
+  });
 };
 
 exports.delete = (req, res) => {
-    Venta.remove(req.params.id, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Not found Venta with id ${req.params.id}.`,
-                });
-            } else {
-                res.status(500).send({
-                    message: "Could not delete Venta with id " + req.params.id,
-                });
-            }
-        } else res.send({ message: `Venta was deleted successfully!` });
-    });
+  Venta.remove(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Venta with id ${req.params.id}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Could not delete Venta with id " + req.params.id,
+        });
+      }
+    } else res.send({ message: `Venta was deleted successfully!` });
+  });
 };
 
 exports.deleteAll = (req, res) => {
-    Venta.removeAll((err, data) => {
-        if (err)
-            res.status(500).send({
-                message: err.message || "Some error occurred while removing all Ventas.",
-            });
-        else res.send({ message: `All Ventas were deleted successfully!` });
-    });
+  Venta.removeAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Ventas.",
+      });
+    else res.send({ message: `All Ventas were deleted successfully!` });
+  });
 };
 
 exports.findAllByIdUser = (req, res) => {
-    
-    Venta.getByIdAdmin(req.params.id, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Not found Ventas with id_admin ${req.params.id}.`,
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving Ventas with id " + req.params.id,
-                });
-            }
-        } else res.send(data);
-    });
+  Venta.getByIdAdmin(req.params.id, (err, data) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        res.status(404).send({
+          message: `Not found Ventas with id_admin ${req.params.id}.`,
+        });
+      } else {
+        res.status(500).send({
+          message: "Error retrieving Ventas with id " + req.params.id,
+        });
+      }
+    } else res.send(data);
+  });
 };
--- app/models/ventas.model.js
+++ app/models/ventas.model.js
@@ -1,140 +1,139 @@
 const sql = require("../config/db.config.js");
 
 // constructor
-const Venta = function(venta) {
-    this.fecha = venta.fecha;
-    this.cliente = venta.cliente;
-    this.estatus = venta.estatus;
-    this.detalles = venta.detalles;
-    this.ingresos = venta.ingresos;
+const Venta = function (venta) {
+  this.cliente = venta.cliente;
+  this.estatus = venta.estatus;
+  this.detalles = venta.detalles;
+  this.ingresos = venta.ingresos;
 };
 
 Venta.create = (newVenta, result) => {
-    sql.query("INSERT INTO ventas SET ?", newVenta, (err, res) => {
-        if (err) {
-            console.log("error : ", err);
-            result(err, null);
-            return;
-        }
-  
-        console.log("created Venta: ", { id: res.insertId, ...newVenta });
-        result(null, { id: res.insertId, ...newVenta });
-    });
+  sql.query("INSERT INTO ventas SET ?", newVenta, (err, res) => {
+    if (err) {
+      console.log("error : ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created Venta: ", { id: res.insertId, ...newVenta });
+    result(null, { id: res.insertId, ...newVenta });
+  });
 };
 
 Venta.getByIdAdmin = (id_admin, result) => {
-    sql.query(
-      `SELECT ventas.*
+  sql.query(
+    `SELECT ventas.*
          FROM ventas
          JOIN usuarios ON ventas.id_admin = usuarios.id
          WHERE ventas.id_admin = ?`,
-      id_admin,
-      (err, res) => {
-        if (err) {
-          console.log("Error: ", err);
-          result(err, null);
-          return;
-        }
-  
-        if (res.length) {
-          console.log("Ventas encontradas: ", res);
-          result(null, res);
-          return;
-        }
-  
-        result({ kind: "not_found" }, null);
+    id_admin,
+    (err, res) => {
+      if (err) {
+        console.log("Error: ", err);
+        result(err, null);
+        return;
       }
-    );
-  };
+
+      if (res.length) {
+        console.log("Ventas encontradas: ", res);
+        result(null, res);
+        return;
+      }
+
+      result({ kind: "not_found" }, null);
+    }
+  );
+};
 
 Venta.findById = (id, result) => {
-    sql.query(`SELECT * FROM ventas WHERE id = ${id}`, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(err, null);
-            return;
-        }
-  
-        if (res.length) {
-            console.log("found venta: ", res[0]);
-            result(null, res[0]);
-            return;
-        }
-  
-        result({ kind: "not_found" }, null);
-    });
+  sql.query(`SELECT * FROM ventas WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found venta: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    result({ kind: "not_found" }, null);
+  });
 };
 
 Venta.getAll = (fecha, result) => {
-    let query = "SELECT * FROM ventas";
-  
-    if (fecha) {
-        query += ` WHERE fecha LIKE '%${fecha}%'`;
+  let query = "SELECT * FROM ventas";
+
+  if (fecha) {
+    query += ` WHERE fecha LIKE '%${fecha}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
     }
-  
-    sql.query(query, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-  
-        console.log("ventas: ", res);
-        result(null, res);
-    });
+
+    console.log("ventas: ", res);
+    result(null, res);
+  });
 };
 
 Venta.updateById = (id, venta, result) => {
-    sql.query(
-        "UPDATE ventas SET fecha = ?, cliente = ?, estatus = ?, detalles = ?, ingresos = ? WHERE id = ?",
-        [venta.fecha, venta.cliente, venta.estatus, venta.detalles, venta.ingresos, id],
-        (err, res) => {
-            if (err) {
-                console.log("error: ", err);
-                result(null, err);
-                return;
-            }
-  
-            if (res.affectedRows == 0) {
-                result({ kind: "not_found" }, null);
-                return;
-            }
-  
-            console.log("updated venta: ", { id: id, ...venta });
-            result(null, { id: id, ...venta });
-        }
-    );
+  sql.query(
+    "UPDATE ventas SET  cliente = ?, estatus = ?, detalles = ?, ingresos = ? WHERE id = ?",
+    [venta.cliente, venta.estatus, venta.detalles, venta.ingresos, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated venta: ", { id: id, ...venta });
+      result(null, { id: id, ...venta });
+    }
+  );
 };
 
 Venta.remove = (id, result) => {
-    sql.query("DELETE FROM ventas WHERE id = ?", id, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-  
-        if (res.affectedRows == 0) {
-            result({ kind: "not_found" }, null);
-            return;
-        }
-  
-        console.log("deleted venta with id: ", id);
-        result(null, res);
-    });
+  sql.query("DELETE FROM ventas WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted venta with id: ", id);
+    result(null, res);
+  });
 };
 
-Venta.removeAll = result => {
-    sql.query("DELETE FROM ventas", (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-  
-        console.log(`deleted ${res.affectedRows} ventas`);
-        result(null, res);
-    });
+Venta.removeAll = (result) => {
+  sql.query("DELETE FROM ventas", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} ventas`);
+    result(null, res);
+  });
 };
 
 module.exports = Venta;
